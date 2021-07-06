1fb742744e1fde19fac44f8e8996202d33d9f7ee
Jeff Chung <drifterz303@gmail.com>
2021-07-06 21:52:40 +0900

    refactor(User, UserDto): 도메인 스스로 일을 처리하도록 수정한다

--- src/main/java/com/toyproject/jobposting/controller/UserController.java
+++ src/main/java/com/toyproject/jobposting/controller/UserController.java
@@ -22,9 +22,7 @@ public class UserController {
         List<UserDto> userDtoList = new ArrayList<>();
         ReadUserResponse<List<UserDto>> response = new ReadUserResponse<>();
         for (User user : userList) {
-            UserDto userDto = new UserDto();
-            userDto.changeToDto(user);
-            userDtoList.add(userDto);
+            userDtoList.add(user.toDto());
         }
         response.setData(userDtoList);
         return response;
@@ -33,11 +31,9 @@ public class UserController {
     @GetMapping("/users/{id}")
     public ReadUserResponse userById(@PathVariable Long id){
         User user = userService.findOne(id);
-        UserDto userDto = new UserDto();
-        userDto.changeToDto(user);
         ReadUserResponse<List<UserDto>> result = new ReadUserResponse<>();
         List<UserDto> userDtoList = new ArrayList<>();
-        userDtoList.add(userDto);
+        userDtoList.add(user.toDto());
         result.setData(userDtoList);
         // null pointer exception 처리
         return result;
@@ -45,18 +41,13 @@ public class UserController {
 
     @PostMapping("/users")
     public void saveUser(@RequestBody @Valid UserDto request){
-        User user = new User();
-        user.changeToUser(request);
-        userService.save(user);
+        userService.save(request.toEntity());
 
     }
 
     @PutMapping("/users/{id}")
     public void editUser(@RequestBody @Valid UserDto request, @PathVariable Long id){
-        User user = new User();
-        user.changeToUser(request);
-
-        userService.updateUser(id, user);
+        userService.updateUser(id, request.toEntity());
     }
 
     @DeleteMapping("/users/{id}")
